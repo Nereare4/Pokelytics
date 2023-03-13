fbcf3e82b1fb2c3cfdcf40db28e1b3e588af5f1f
Kino <joaquin.ayllongar@gmail.com>
2023-03-13 17:38:23 +0100

    init

---
+++ Pokelytics/app/src/main/java/study/project/pokelytics/MainActivity.kt
@@ -0,0 +1,11 @@
+package study.project.pokelytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
