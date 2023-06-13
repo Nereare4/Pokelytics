ea7b39b5d2925f8d09f7206b955d5252c9a4f4ef
Nereare4 <nereare4@gmail.com>
2023-06-13 19:51:39 +0200

    Modified the alert dialog when the user is not logged, the default name, added to shared preferences the favlist and team

--- Pokelytics/app/src/main/java/study/project/pokelytics/fragments/info/InfoPokemonFragment.kt
+++ Pokelytics/app/src/main/java/study/project/pokelytics/fragments/info/InfoPokemonFragment.kt
@@ -1,9 +1,11 @@
 package study.project.pokelytics.fragments.info
 
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import study.project.pokelytics.R
+import study.project.pokelytics.activities.ActivityBase
 import study.project.pokelytics.activities.InfoActivity
 import study.project.pokelytics.api.model.Pokemon
 import study.project.pokelytics.databinding.FragmentInfoPokemonBinding
@@ -122,7 +124,7 @@ class InfoPokemonFragment: FragmentBase<FragmentInfoPokemonBinding>() {
         return object : PokemonInterface {
             override fun onFavoriteClick(pokemon: Pokemon) {
                 if (User.getInstance().email.isEmpty()) {
-                    Toast.makeText(context, "Please login to add favorites", Toast.LENGTH_SHORT).show()
+                    alertFavsTeam()
                     return
                 }
                 if (User.getInstance().addFav(pokemon)) {
@@ -139,7 +141,7 @@ class InfoPokemonFragment: FragmentBase<FragmentInfoPokemonBinding>() {
 
             override fun onTeamClick(pokemon: Pokemon) {
                 if (User.getInstance().email.isEmpty()) {
-                    Toast.makeText(context, "Please login to add pokemon to team", Toast.LENGTH_SHORT).show()
+                    alertFavsTeam()
                     return
                 }
                 if (User.getInstance().addTeam(pokemon)) {
@@ -160,5 +162,16 @@ class InfoPokemonFragment: FragmentBase<FragmentInfoPokemonBinding>() {
                 return moreInfoViewModel
             }
         }
+    }
+    fun alertFavsTeam(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(resources.getString(R.string.loginTitle))
+        builder.setMessage(resources.getString(R.string.loginQuestion))
+        builder.setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+            (activity as ActivityBase<*>).navigator.goToLogin()
+            activity?.finish()
+        }
+        builder.setNegativeButton(resources.getString(R.string.cancel)) { dialog, which ->}
+        builder.show()
     }
 }
